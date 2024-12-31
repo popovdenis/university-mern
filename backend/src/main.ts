afdc7fa5a00a44cb73751b6db2d6124fee92377a
denis.popov <denispopov2112@gmail.com>
2024-12-31 18:19:01 +0100

    Sign In / Sign Up

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,20 @@ import * as path from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.useStaticAssets(path.join(__dirname, '..', 'uploads'));
+  app.use('/uploads', (req, res, next) => {
+    const allowedOrigins = ['http://localhost:3000'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+      res.setHeader('Access-Control-Allow-Origin', origin);
+      res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+      res.setHeader('Access-Control-Allow-Credentials', 'true');
+    }
+    next();
+  });
+  app.useStaticAssets(path.join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads',
+  });
   app.enableCors({
     origin: 'http://localhost:3000',
     credentials: true,
