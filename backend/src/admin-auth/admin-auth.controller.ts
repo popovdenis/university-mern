31cd7de4775ed9b779a79183106d8dd8af95e6c8
denis.popov <denispopov2112@gmail.com>
2025-01-03 23:22:48 +0100

    Auth

--- backend/src/admin-auth/admin-auth.controller.ts
+++ backend/src/admin-auth/admin-auth.controller.ts
@@ -1,7 +1,8 @@
-import {Body, Controller, Get, Post, UnauthorizedException} from '@nestjs/common';
-import { AdminAuthService } from './admin-auth.service';
+import {Body, Controller, Get, Post, UnauthorizedException, UseGuards} from '@nestjs/common';
+import {AdminAuthService} from './admin-auth.service';
 import {AdminDto} from "../admin/admin-user/dto/admin.dto";
 import {LoginAdminDto} from "../admin/admin-user/dto/login-admin.dto";
+import {JwtAuthGuard} from "./jwt-auth.guard";
 
 @Controller('admin-auth')
 export class AdminAuthController {
@@ -9,7 +10,7 @@ export class AdminAuthController {
 
     @Post('login')
     async login(@Body() loginDto: LoginAdminDto) {
-        const { email, password } = loginDto;
+        const {email, password} = loginDto;
         const admin = await this.adminAuthService.validateAdmin(email, password);
 
         if (!admin) {
@@ -17,17 +18,23 @@ export class AdminAuthController {
         }
 
         const token = this.adminAuthService.generateJwt(admin);
-        return { message: 'Login successful', token };
+        return {message: 'Login successful', token};
     }
 
     @Post('register')
     async register(@Body() registerDto: AdminDto) {
         const admin = await this.adminAuthService.register(registerDto);
-        return { message: 'Admin has been registered successfully', admin };
+        return {message: 'Admin has been registered successfully', admin};
     }
 
     @Get()
     async index() {
-        return { message: 'Hello World from /admin-auth' };
+        return {message: 'Hello World from /admin-auth'};
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('protected')
+    async protectedRoute() {
+        return { message: 'You have accessed a protected route!' }
     }
 }
